bc5605b6dd5449a3ff0230d464baff7dd6e91684
Ravi Pansuriya <pansuriyaravi.rc@gmail.com>
2022-04-27 11:39:51 +0530

    Initial Commit

---
+++ CountryCode/CountryCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryCodeApp.swift
+//  CountryCode
+//
+//  Created by Zeref on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CountryCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
