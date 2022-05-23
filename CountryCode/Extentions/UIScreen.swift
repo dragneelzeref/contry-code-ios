0a0eff417e0c7ee5e7078250aab01080ee768e8d
Ravi Pansuriya <pansuriyaravi.rc@gmail.com>
2022-05-23 11:18:33 +0530

    UIScreen extension for width and height added

---
+++ CountryCode/Extentions/UIScreen.swift
@@ -0,0 +1,18 @@
+//
+//  UIScreen.swift
+//  CountryCode
+//
+//  Created by Zeref on 27/04/22.
+//
+
+import SwiftUI
+
+extension UIScreen{
+    static var screenWidth: CGFloat{
+        return UIScreen.main.bounds.width
+    }
+    
+    static var screenHeight: CGFloat{
+        return UIScreen.main.bounds.height
+    }
+}
